5ceea5ccb85626033dd9f89e1b9fe7808c776e63
Syeda Imaan Zahra <zahrasyedaimaan@gmail.com>
2024-04-19 11:28:08 -0700

    created cli word counter

---
+++ index.ts
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+let answer:
+{
+    Sentence: string
+}
+ = await inquirer.prompt
+(
+    [
+        {
+           name:"Sentence",
+           type:"input",
+           message:"Enter your sentence to count the word" 
+        }
+    ]
+)
+
+const words = answer.Sentence.trim().split(" ")
+
+console.log(words)
+
+console.log(`Your sentence word count is ${words.length}`);
